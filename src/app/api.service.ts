f0b8472f96d8ab0ef03fe20cca79745f0dc3ea93
chegejohn159 <chegejohn159@gmail.com>
2022-12-05 11:07:27 +0300

    servuce

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  baseURL: any = `${environment.prodAPI}`;
+  create(data: any): Observable<any> {
+    let API_URL = `${this.baseURL}/report/add`;
+    return this.http.post(API_URL, data, { headers: this.headers, withCredentials: false }).pipe(map(res => {
+      return res || {}
+    }),
+      catchError(this.errorMgmt)
+    )
+  }
+  get(): Observable<any> {
+    let API_URL = `${this.baseURL}/report/all`;
+    return this.http.get(API_URL, { headers: this.headers, withCredentials: false }).pipe(map(res => {
+      return res || {}
+    }),
+      catchError(this.errorMgmt)
+    )
+  }
+  update(data: any, id: number): Observable<any> {
+    let API_URL = `${this.baseURL}/update/${id}`;
+    return this.http.put(API_URL, data, { headers: this.headers, withCredentials: false }).pipe(map(res => {
+      return res || {}
+    }),
+      catchError(this.errorMgmt)
+    )
+  }
+  delete(id: number): Observable<any> {
+    let API_URL = `${this.baseURL}/delete/${id}`;
+    return this.http.delete(API_URL, { headers: this.headers, withCredentials: false }).pipe(map(res => {
+      return res || {}
+    }),
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Error handling
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `${error.error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+
 }
+
+
